aa2efaf1bb451dd45932d94f4097247193f2cefe
hexdecimals <21987529+hexdecimals@users.noreply.github.com>
2023-05-07 14:06:42 +0530

    fix: removed redundant code & misc

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -3,7 +3,6 @@
 import * as React from "react"
 import { useTheme } from "next-themes"
 
-import { Button } from "@/components/ui/button"
 import { Icons } from "@/components/icons"
 
 export function ThemeToggle() {
@@ -14,8 +13,8 @@ export function ThemeToggle() {
       className="inline-flex"
       onClick={() => setTheme(theme === "light" ? "dark" : "light")}
     >
-      <Icons.sun className="rotate-0 scale-100  transition-all dark:-rotate-90 dark:scale-0" />
-      <Icons.moon className="absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+      <Icons.sun className="dark:hidden" />
+      <Icons.moon className="hidden dark:block" />
       <span className="sr-only">Toggle theme</span>
     </button>
   )
